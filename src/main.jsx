52068c5b9e8cd88fb275aa6e3b08d4ec43656da3
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-04 23:29:56 +0100

    fix: activate fast WordPress remediation batch guard

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './remediationIntegrity';
 import './locationEvents';
 import './seoResponseIntegrity';
 import './remediationReportUx';
+import './remediationBatchGuard';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
