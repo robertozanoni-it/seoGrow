aef97bcecd0c848a04fe67613b95a91beee308c0
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-04 17:55:13 +0100

    fix: clarify remediation report outcomes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './remediationVerificationMigration';
 import './remediationIntegrity';
 import './locationEvents';
 import './seoResponseIntegrity';
+import './remediationReportUx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
