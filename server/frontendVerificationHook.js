2eafbfa30944af050932db6c3259691443657ce9
Roberto Zanoni <cod.zanoni@gmail.com>
2026-09-05 18:45:31 +0100

    refactor: remove frontend verification express monkey patches

--- server/frontendVerificationHook.js
+++ server/frontendVerificationHook.js
@@ -1,10 +1,7 @@
 import dns from "node:dns/promises";
 import net from "node:net";
-import express from "express";
 
 const HOOKED = Symbol.for("seogrow.frontendVerificationHook");
-const USE_PATCHED = Symbol.for("seogrow.frontendVerificationUsePatched");
-const LISTEN_PATCHED = Symbol.for("seogrow.frontendVerificationListenPatched");
 
 function privateAddress(address) {
   if (net.isIPv4(address)) {
@@ -289,22 +286,4 @@ function registerRoutes(app) {
   });
 }
 
-const originalUse = express.application.use;
-if (!originalUse[USE_PATCHED]) {
-  const patchedUse = function (...args) {
-    if (!this[HOOKED] && args[0] === "/api") registerRoutes(this);
-    return originalUse.apply(this, args);
-  };
-  patchedUse[USE_PATCHED] = true;
-  express.application.use = patchedUse;
-}
-
-const originalListen = express.application.listen;
-if (!originalListen[LISTEN_PATCHED]) {
-  const patchedListen = function (...args) {
-    registerRoutes(this);
-    return originalListen.apply(this, args);
-  };
-  patchedListen[LISTEN_PATCHED] = true;
-  express.application.listen = patchedListen;
-}
+export { registerRoutes };
